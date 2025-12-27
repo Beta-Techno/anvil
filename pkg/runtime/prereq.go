7c27ea829093ce0230758a083512d49072e029dd
nickbendasg <nick@bendas.info>
2025-12-27 15:21:07 -0500

    Fallback to sops binary install when apt fails

--- pkg/runtime/prereq.go
+++ pkg/runtime/prereq.go
@@ -2,8 +2,12 @@ package runtime
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"runtime"
 )
 
 var requirements = []struct {
@@ -22,8 +26,16 @@ func CheckPrereqs() error {
 		if _, err := exec.LookPath(req.cmd); err == nil {
 			continue
 		}
-		if err := installViaApt(req.pkg, &aptUpdated); err != nil {
-			return fmt.Errorf("required command %s not found and automatic install failed: %w", req.cmd, err)
+
+		var installErr error
+		if req.cmd == "sops" {
+			installErr = installSops(&aptUpdated)
+		} else {
+			installErr = installViaApt(req.pkg, &aptUpdated)
+		}
+
+		if installErr != nil {
+			return fmt.Errorf("required command %s not found and automatic install failed: %w", req.cmd, installErr)
 		}
 	}
 	return nil
@@ -47,6 +59,74 @@ func installViaApt(packageName string, aptUpdated *bool) error {
 	}
 
 	return runWith(runner, "apt-get", "install", "-y", packageName)
+}
+
+func installSops(aptUpdated *bool) error {
+	if err := installViaApt("sops", aptUpdated); err == nil {
+		return nil
+	}
+	return installSopsFromRelease()
+}
+
+const sopsVersion = "3.9.4"
+
+func installSopsFromRelease() error {
+	arch := runtime.GOARCH
+	var suffix string
+	switch arch {
+	case "amd64":
+		suffix = "amd64"
+	case "arm64":
+		suffix = "arm64"
+	default:
+		return fmt.Errorf("unsupported architecture %s for sops binary install", arch)
+	}
+
+	url := fmt.Sprintf("https://github.com/getsops/sops/releases/download/v%[1]s/sops-v%[1]s.linux.%s", sopsVersion, suffix)
+	fmt.Println("[runtime] downloading sops from", url)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("download failed: %s", resp.Status)
+	}
+
+	tmp, err := os.CreateTemp("", "sops-binary-*")
+	if err != nil {
+		return err
+	}
+	defer os.Remove(tmp.Name())
+
+	if _, err := io.Copy(tmp, resp.Body); err != nil {
+		return err
+	}
+	tmp.Close()
+	if err := os.Chmod(tmp.Name(), 0o755); err != nil {
+		return err
+	}
+
+	target := "/usr/local/bin/sops"
+	runner := buildRunner()
+	if runner == nil {
+		if os.Geteuid() != 0 {
+			return fmt.Errorf("need sudo or root to install sops")
+		}
+		if err := os.MkdirAll(filepath.Dir(target), 0o755); err != nil {
+			return err
+		}
+		return os.Rename(tmp.Name(), target)
+	}
+
+	if err := runCommand("chmod", "+x", tmp.Name()); err != nil {
+		return err
+	}
+	if err := runner("install", "-m", "755", tmp.Name(), target); err != nil {
+		return err
+	}
+	return nil
 }
 
 type runnerFunc func(name string, args ...string) error
